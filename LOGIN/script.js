ddfce2700fd5a37a10cef076a8a034c8eb392286
MrBl4ck <franz.carvajal04@gmail.com>
2024-03-14 01:00:06 -0400

    RollBack

--- LOGIN/script.js
+++ LOGIN/script.js
@@ -1,39 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const container = document.getElementById('container');
-    const registerBtn = document.getElementById('register');
-    const loginBtn = document.getElementById('login');
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
-    registerBtn.addEventListener('click', () => {
-        container.classList.add("active");
-    });
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
 
-    loginBtn.addEventListener('click', () => {
-        container.classList.remove("active");
-    });
-
-    function onSuccess(googleUser) {
-        console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-        // Aquí puedes redirigir al usuario o hacer cualquier otra cosa necesaria tras el inicio de sesión
-    }
-
-    function onFailure(error) {
-        console.log(error);
-    }
-
-    function renderButton() {
-        gapi.signin2.render('customBtn', {
-            'scope': 'profile email',
-            'width': 240,
-            'height': 50,
-            'longtitle': true,
-            'theme': 'dark',
-            'onsuccess': onSuccess,
-            'onfailure': onFailure
-        });
-    }
-
-    gapi.load('auth2', function() {
-        renderButton();
-        gapi.auth2.init();
-    });
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
 });
